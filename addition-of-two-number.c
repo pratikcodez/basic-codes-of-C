df63dbb315d162ee68454dc4af901c7490d7595e
-kraven <pratik2007.shelke@gmail.com>
2026-09-11 01:29:51 +0530

    Add program to sum two numbers in C

---
+++ addition-of-two-number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    //intializing three variables
+    int n1, n2, sum;
+    //taking input from user 
+    printf ("Enter two numbers to add");
+    scanf("%d %d", &n1, &n2);
+    //adding two numbers
+    sum = n1 + n2;
+    //ddisplaying the sum
+    printf ("Sum of two numbers is : %d", sum);
+    return 0;
+
+
+}
